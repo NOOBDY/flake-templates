b9b1943a92982ea2135fda26875b8c38b4374172
noobdy <htommyf12@gmail.com>
2025-06-13 03:47:46 +0800

    added haskell template

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
     outputs = { self, ... }: {
         templates = {
-            
+            haskell = {
+                path = ./templates/haskell;
+                description = "Haskell Dev Environment";
+            };
         };
     };
 }
